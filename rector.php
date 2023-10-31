40bde38123e66140f77cb18f220e4b32dc75cfb7
Joshua Bours <joshua.bours@servexs.nl>
2023-10-31 15:46:18 +0100

    updated rector config

--- rector.php
+++ rector.php
@@ -4,14 +4,11 @@ use Rector\Config\RectorConfig;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->paths([
-        __DIR__ . '/DataTransformer',
-        __DIR__ . '/DependencyInjection',
-        __DIR__ . '/Resources',
-        __DIR__ . '/Type',
+        __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
     $rectorConfig->sets([
         \Rector\Set\ValueObject\LevelSetList::UP_TO_PHP_74,
-        //        \Rector\Symfony\Set\SymfonyLevelSetList::UP_TO_SYMFONY_50,
+//      \Rector\Symfony\Set\SymfonyLevelSetList::UP_TO_SYMFONY_54,
     ]);
 };
